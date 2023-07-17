75ec0c713e338061801a0d49328b99b70fcd0294
Oleksii Popov <popov.scar@gmail.com>
2023-07-17 23:23:22 +0300

    View banner and menu. Environment file.

--- run.py
+++ run.py
@@ -1,4 +1,22 @@
 import requests
+import os
+from dotenv import load_dotenv
+import sys
+from view.view import View
+
+load_dotenv()
+
+view = View()
+view.separetor()
+view.showBanner()
+view.separetor()
+view.showMenu()
+view.separetor()
+menuNumber: str = view.proposeChoose()
+
+print(menuNumber)
+
+exit()
 
 response = requests.get('https://stores-api.zakaz.ua/stores')
 data = response.json()
